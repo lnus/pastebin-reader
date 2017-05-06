6b42374a890ba6a4c5ca60f66a35473fdd978b53
Linus <linuzlp@gmail.com>
2017-05-06 15:07:23 +0200

    :circus_tent: Fixed formatting

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import requests, random
+import requests
+import random
 from bs4 import BeautifulSoup
 
 class PasteFetcher():
@@ -7,8 +8,8 @@ class PasteFetcher():
 
     def get_archive(self):
         pastes = []
-        plainArchive = requests.get(self.URL.format("archive")).text
-        soup = BeautifulSoup(plainArchive, "html.parser")
+        plain_archive = requests.get(self.URL.format("archive")).text
+        soup = BeautifulSoup(plain_archive, "html.parser")
         for td in soup.find_all("td"):
             try:
                 pastes.append(td.find("a")["href"])
@@ -16,15 +17,15 @@ class PasteFetcher():
                 pass
         return pastes    
 
-    def paste_parser(self, pasteID = "pfcWEYiL"):
-        plainPaste = requests.get(self.URL.format(pasteID)).text
-        soup = BeautifulSoup(plainPaste, "html.parser")
-        fullPaste = soup.find("textarea")
-        parsed = fullPaste.string
+    def parse_paste(self, paste_id = "pfcWEYiL"):
+        plain_paste = requests.get(self.URL.format(paste_id)).text
+        soup = BeautifulSoup(plain_paste, "html.parser")
+        full_paste = soup.find("textarea")
+        parsed = full_paste.string
         return parsed
 
     def random_paste(self):
         pastes = self.get_archive()
-        pasteID = random.choice(pastes)
-        final = self.paste_parser(pasteID)
+        paste_id = random.choice(pastes)
+        final = self.parse_paste(paste_id)
         return final 
